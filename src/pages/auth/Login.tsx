140a333743989190462c2debfa068c0a382c659b
Thi Pham <thipham@This-MacBook-Pro.local>
2025-08-26 22:27:13 +0700

    🌻260825: responsive layout login user

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,12 +1,11 @@
 const Login = () => {
   return (
-    <div className="bg-auth bg-cover bg-no-repeat w-full h-screen">
-      <div className="bg-navy-50/50 w-full h-screen relative">
+    <div className="bg-auth bg-cover bg-no-repeat w-full h-screen ">
+      <div className="bg-navy-50/50 w-full h-screen flex justify-center items-center px-7">
         <div
-          className="w-[495px] h-[330px] bg-white border-light-100 rounded-20 
-                        py-10 px-[25px] shadow-2xs flex flex-col justify-center items-center gap-9 
-                        absolute top-2/4 left-2/4 -translate-x-[50%] -translate-y-[50%] 
-                        font-figtree text-[28px]"
+          className="w-full sm:w-[495px] h-[300px] sm:h-[330px] bg-white border-light-100 rounded-20 
+                        py-5 sm:py-10 px-[25px] shadow-xs flex flex-col justify-center items-center gap-6 sm:gap-9 
+                        font-figtree text-xl sm:text-[28px]"
         >
           <div className="w-full h-[30px] flex justify-center items-center gap-1.5">
             <img src="./icons/logo.svg" alt="logo" width="30px" height="30px" />
@@ -16,13 +15,15 @@ const Login = () => {
           </div>
           <div className="w-full flex flex-col gap-3 items-center text-center">
             <div className="font-semibold">Start Your Travel Journey</div>
-            <div className="font-normal text-[18px] text-gray-100">
+            <div className="font-normal text-base sm:text-[18px] text-gray-100">
               Sign in to explore custom itineraries, trending destinations, and
               much more
             </div>
           </div>
-          <button className="w-full h-[52px] bg-primary-100 rounded-lg border-2 border-primary-100 px-4 py-3.5 shadow-xs
-                            text-white font-figtree font-semibold text-lg/[18px] hover:bg-primary-500">
+          <button
+            className="w-full sm:h-[52px] bg-primary-100 rounded-lg border-2 border-primary-100 px-4 py-1.5 sm:py-3.5 shadow-xs
+                            text-white font-figtree font-semibold text-base sm:text-lg/[18px] hover:bg-primary-500"
+          >
             Sign in
           </button>
         </div>
