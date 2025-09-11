6c058c5d7cba055867c2fd7a491cba5123733b8f
Shirley <shirley.pham@terralogic.com>
2025-09-11 15:33:05 +0700

    110925: finished featured destinations -no responsive

--- src/pages/user/Homepage.tsx
+++ src/pages/user/Homepage.tsx
@@ -14,82 +14,120 @@ const Homepage = () => {
         </div>
 
         {/* Grid imgs */}
-        <div className="flex gap-[30px]">
-          <div className="w-[870px] flex flex-col gap-[30px]">
-            {/* Featured card */}
-            <div className="w-full h-[297px] p-[30px] bg-card-1 bg-cover bg-center bg-no-repeat rounded-[14px] flex flex-col gap-2.5 justify-between">
-              <div
-                className="w-[65px] h-7 px-[19px] py-0.5 rounded-20 bg-white
-                              font-bold text-lg text-red-100 leading-6 shadow-50"
-              >
+        <div className="w-full h-[717px] flex gap-30 ">
+          {/* Col 1 */}
+          <div className="h-full grow flex flex-col gap-30">
+            {/* Row 1 */}
+            <div className="h-[297px] bg-card-1 bg-cover bg-no-repeat rounded-14 p-30 flex flex-col justify-between">
+              <div className="bg-white w-[65px] h-7 rounded-20 px-[19px] py-0.5 shadow-50  text-red-100 font-bold text-lg leading-6">
                 3.5
               </div>
-              <div className="w-full flex flex-col gap-3.5 text-white">
-                <h1 className="font-bold text-[30px]">Barcelona Tour</h1>
-                <div className="flex gap-2.5 w-fit">
-                  <img
-                    src="./images/david.webp"
-                    alt=""
-                    width="28.57px"
-                    className="rounded-full"
-                  />
-                  <p className="text-lg">196 Activities</p>
+              <div className="text-white flex flex-col gap-3.5">
+                <h3 className="font-bold text-3xl">Barcelona Tour</h3>
+                <div className="w-full flex gap-2.5">
+                  <div className="avatar">
+                    <div className="w-7 rounded-full">
+                      <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+                    </div>
+                  </div>
+                  <span className="text-lg">196 Activities</span>
                 </div>
               </div>
             </div>
 
-            <div className="h-[390px] flex justify-between">
-              <div className="w-[420px] h-[390px] p-[30px] bg-card-2 bg-cover bg-center bg-no-repeat rounded-[14px] flex flex-col gap-2.5 justify-between">
-                <div
-                  className="w-[65px] h-7 px-[19px] py-0.5 rounded-20 bg-white
-                              font-bold text-lg text-red-100 leading-6 shadow-50"
-                >
+            {/* Row 2 */}
+            <div className="w-auto h-[390px] flex gap-30">
+              <div className="grow bg-card-2 bg-cover bg-no-repeat rounded-14 py-5 px-30 flex flex-col justify-between">
+                <div className="bg-white w-[65px] h-7 rounded-20 px-[19px] py-0.5 shadow-50  text-red-100 font-bold text-lg leading-6">
                   3.5
                 </div>
-                <div className="w-full flex flex-col gap-3.5 text-white">
-                  <h1 className="font-bold text-[30px]">
-                    London, United State
-                  </h1>
-                  <div className="flex gap-2.5 w-fit">
-                    <img
-                      src="./images/david.webp"
-                      alt=""
-                      width="28.57px"
-                      className="rounded-full"
-                    />
-                    <p className="text-lg">196 Activities</p>
+                <div className="text-white flex flex-col gap-3.5">
+                  <h3 className="font-bold text-3xl">London, United State</h3>
+                  <div className="w-full flex items-center gap-2.5">
+                    <div className="avatar">
+                      <div className="w-5 h-5 rounded-full">
+                        <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+                      </div>
+                    </div>
+                    <span className="text-lg">196 Activities</span>
                   </div>
                 </div>
               </div>
-              <div className="w-[420px] h-[390px] p-[30px] bg-card-3 bg-cover bg-center bg-no-repeat rounded-[14px] flex flex-col gap-2.5 justify-between">
-                <div
-                  className="w-[65px] h-7 px-[19px] py-0.5 rounded-20 bg-white
-                              font-bold text-lg text-red-100 leading-6 shadow-50"
-                >
-                  3.5
-                </div>
-                <div className="w-full flex flex-col gap-3.5 text-white">
-                  <h1 className="font-bold text-[30px]">Austrailia Tour</h1>
-                  <div className="flex gap-2.5 w-fit">
-                    <img
-                      src="./images/david.webp"
-                      alt=""
-                      width="28.57px"
-                      className="rounded-full"
-                    />
-                    <p className="text-lg">196 Activities</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
 
-          <div className="w-[270px] flex flex-col justify-between gap-[20px]">
-            <div className="w-full h-[226px] bg-card-4 bg-cover bg-no-repeat rounded-[14px]">
-              
+              <div className="grow bg-card-3 bg-cover bg-no-repeat rounded-14 py-5 px-30 flex flex-col justify-between">
+                <div className="bg-white w-[65px] h-7 rounded-20 px-[19px] py-0.5 shadow-50  text-red-100 font-bold text-lg leading-6">
+                  3.5
+                </div>
+                <div className="text-white flex flex-col gap-3.5">
+                  <h3 className="font-bold text-3xl">London, United State</h3>
+                  <div className="w-full flex items-center gap-2.5">
+                    <div className="avatar">
+                      <div className="w-5 h-5 rounded-full">
+                        <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+                      </div>
+                    </div>
+                    <span className="text-lg">196 Activities</span>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="w-full h-[226px] bg-card-5 bg-cover bg-no-repeat rounded-[14px]"></div>
-            <div className="w-full h-[226px] bg-card-6 bg-cover bg-no-repeat rounded-[14px]"></div>
+          </div> 
+
+          {/* Col 2 */}
+          <div className="w-[270px] h-full flex flex-col justify-between">
+            {/* Row 1 */}
+            <div className="w-full h-[226px] rounded-20 py-3.5 px-5 bg-card-4 bg-cover bg-no-repeat flex flex-col justify-between items-start gap-2.5">
+              <div className="bg-white w-[65px] h-7 rounded-20 px-[19px] py-0.5 shadow-50  text-red-100 font-bold text-sm leading-6 flex justify-center">
+                3.5
+              </div>
+              <div className="text-white flex flex-col gap-2">
+                <h3 className="font-semibold text-lg">Australia Tour</h3>
+                <div className="w-full flex gap-2.5">
+                  <div className="avatar">
+                    <div className="w-4 rounded-full">
+                      <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+                    </div>
+                  </div>
+                  <span className="text-xs">196 Activities</span>
+                </div>
+              </div>
+            </div>
+
+            {/* Row 2 */}
+            <div className="w-full h-[226px] rounded-20 py-3.5 px-5 bg-card-5 bg-cover bg-no-repeat flex flex-col justify-between items-start gap-2.5">
+              <div className="bg-white w-[65px] h-7 rounded-20 px-[19px] py-0.5 shadow-50  text-red-100 font-bold text-sm leading-6 flex justify-center">
+                3.5
+              </div>
+              <div className="text-white flex flex-col gap-2">
+                <h3 className="font-semibold text-lg">Japan Tour</h3>
+                <div className="w-full flex gap-2.5">
+                  <div className="avatar">
+                    <div className="w-4 rounded-full">
+                      <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+                    </div>
+                  </div>
+                  <span className="text-xs">196 Activities</span>
+                </div>
+              </div>
+            </div>
+
+            {/* Row 3 */}
+            <div className="w-full h-[226px] rounded-20 py-3.5 px-5 bg-card-6 bg-cover bg-no-repeat flex flex-col justify-between items-start gap-2.5">
+              <div className="bg-white w-[65px] h-7 rounded-20 px-[19px] py-0.5 shadow-50  text-red-100 font-bold text-sm leading-6 flex justify-center">
+                3.5
+              </div>
+              <div className="text-white flex flex-col gap-2">
+                <h3 className="font-semibold text-lg">Australia Tour</h3>
+                <div className="w-full flex gap-2.5">
+                  <div className="avatar">
+                    <div className="w-4 rounded-full">
+                      <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+                    </div>
+                  </div>
+                  <span className="text-xs">196 Activities</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
